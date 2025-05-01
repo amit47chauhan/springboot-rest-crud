bf5249d508c6926fc04afcb14002350033b995c5
amit47chauhan <chauhanamit.ak47@gmail.com>
2025-05-01 09:21:01 +0530

    Delete an Employee

--- src/main/java/com/learn/crud/rest/EmployeeRestController.java
+++ src/main/java/com/learn/crud/rest/EmployeeRestController.java
@@ -98,5 +98,19 @@ public class EmployeeRestController {
         return objectMapper.convertValue(employeeNode, Employee.class);
     }
 
+    //Delete an employee
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null) {
+            throw new RuntimeException("Employee id not found "+ employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
 
 }
